bf699b0856dba5d116b3cdc289f98fa928de237e
Carlos <carloshin98@gmail.com>
2023-03-23 21:03:19 +0100

    add tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
+import { config } from './_config'
+
+export default function Home() {
+  const [users, setUsers] = useState([])
+  const getUsers = async () => {
+    const url = `${config.backendUrl}/public/users`;
+    const res = await axios.get(url, {
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+    setUsers(res.data);
+  }
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  return (
+    <div className='p-8'>
+      <h1 className="text-2xl text-black-600">
+        Users
+      </h1>
+      <ul>
+
+        {users.map(user => {
+          return <li key={user.name} className="">
+            {user.name}
+          </li>
+        })}
+      </ul>
+
+    </div>
+  )
+}
